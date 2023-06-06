128cbb79b2a8b00c07e8e9722c721fbbbd2a8c27
onkelhoy <onkelhoy@gmail.com>
2023-06-06 23:52:45 +0200

    feat: new atoms component : form

---
+++ packages/atoms/form/test/component.test.js
@@ -0,0 +1,19 @@
+import { fixture } from '@circular-tools/test';
+
+describe('Form', function () {
+  describe('base tests', function () {
+    it('web-component should exists', function () {
+      const elm = fixture('o-form');
+
+      if (!elm) {
+        throw new Error('element not created')
+      }
+
+      const docelm = document.querySelector(`o-form[data-testid="${elm.getAttribute('data-testid')}"]`);
+      
+      if (!docelm) {
+        throw new Error('element not found');
+      }
+    });
+  });
+});
