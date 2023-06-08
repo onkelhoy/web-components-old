5e2ed074e8f64e96e3d5f8e9cae5c7a483e40a56
onkelhoy <onkelhoy@gmail.com>
2023-06-08 23:01:03 +0200

    fix: analyze respects document-fragment

--- scripts/analyse/util/html.js
+++ scripts/analyse/util/html.js
@@ -17,7 +17,12 @@ function build_html(package_dir, component_name, script_dir) {
 
           window.COMPONENT_HTML_EXTRACTOR_FUNCTION = () => {
             const temp_component = new ${component_name}();
-            return temp_component.render();
+            const html = temp_component.render();
+
+            
+            if ('children' in html) return Array.from(html.children).map(node => node.outerHTML).join(' ')
+            
+            return html;
           }
         </script>
       </head>
