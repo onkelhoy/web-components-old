1434859e698b019f050b763cbefc9bd3db890206
onkelhoy <onkelhoy@gmail.com>
2023-06-11 10:36:11 +0200

    feat: new atoms component : editor

---
+++ packages/atoms/editor/src/register.ts
@@ -0,0 +1,12 @@
+import { Editor } from './component.js';
+
+// Register the element with the browser
+const cElements = customElements ?? window?.customElements;
+
+if (!cElements) {
+  throw new Error('Custom Elements not supported');
+}
+
+if (!cElements.get('o-editor')) {
+  cElements.define('o-editor', Editor);
+}
