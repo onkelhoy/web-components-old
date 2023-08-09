2c974f9361fbce52fd15e1d9c710f5bd3ac00407
onkelhoy <onkelhoy@gmail.com>
2023-08-09 14:25:33 +0200

    fix: introduced nvmrc
    fix: node version, node script
    
    wip: npm ci moved into individual
    
    fix: update build script to have CI mode

--- scripts/build/run.sh
+++ scripts/build/run.sh
@@ -2,15 +2,30 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "Global semantic versioning?"
-echo "answer:"
-echo "[1] none"
-echo "[2] patch"
-echo "[3] minor"
-echo "[4] major"
+# Initialize the flag as not set
+CI_FLAG=0
 
-read -p "Enter the number corresponding to your choice: " choice
+# Check each argument
+for arg in "$@"; do
+  if [ "$arg" == "-ci" ]; then
+    CI_FLAG=1
+  fi
+done
 
-node $SCRIPT_DIR/main.js $choice $1
+# If the flag is set, run the command
+if [ $CI_FLAG -eq 1 ]; then
+  echo "Global semantic versioning?"
+  echo "answer:"
+  echo "[1] none"
+  echo "[2] patch"
+  echo "[3] minor"
+  echo "[4] major"
+
+  read -p "Enter the number corresponding to your choice: " choice
+
+  node $SCRIPT_DIR/main.js $choice $1
+else
+  node $SCRIPT_DIR/main.js $choice 1
+fi
 
 echo "packages built"
