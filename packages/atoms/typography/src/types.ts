eae064aeb87862baf9dbffc1cbc9701308d98bba
onkelhoy <onkelhoy@gmail.com>
2023-10-07 13:16:47 +0200

    fix: typography styles updated

--- packages/atoms/typography/src/types.ts
+++ packages/atoms/typography/src/types.ts
@@ -1,2 +1,3 @@
-export type Variant = `${'C'|'T'|'H'}${1|2|3|4}`|'H5';
+type CommonVariantNames = 'C'|'T'|'H'|'copy'|'title'|'heading';
+export type Variant = `${CommonVariantNames|'B'|'button'}${1|2}`|`${CommonVariantNames}${3|4}`|'H5';
 export type Alignment = "center" | "justify" | "start" | "end" | "left" | "right" | "unset" | "inherit" | "initial";
