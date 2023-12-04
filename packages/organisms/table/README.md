ec40394d39169ba12d0563766c3c3642e0c76c13
onkelhoy <onkelhoy@gmail.com>
2023-12-04 11:05:00 +0100

    feat: new organisms component : table

---
+++ packages/organisms/table/README.md
@@ -0,0 +1,11 @@
+# Table
+
+Atomic Type: organisms
+
+Version: 0.0.0
+
+## Development
+
+Development servers can be started and should all exist inside `"views"` folder
+
+## Scripts
---
+++ packages/organisms/table/src/register.ts
@@ -0,0 +1,12 @@
+import { Table } from './component.js';
+
+// Register the element with the browser
+const cElements = customElements ?? window?.customElements;
+
+if (!cElements) {
+  throw new Error('Custom Elements not supported');
+}
+
+if (!cElements.get('pap-table')) {
+  cElements.define('pap-table', Table);
+}
