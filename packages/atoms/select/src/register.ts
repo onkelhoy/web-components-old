15dacd355fadffb023f6499e059198f5dd02e72e
onkelhoy <onkelhoy@gmail.com>
2024-02-22 14:03:18 +0100

    fix: register file got removed - why?

---
+++ packages/atoms/select/src/register.ts
@@ -0,0 +1,16 @@
+import { Option } from './components/option';
+import { Select } from './component.js';
+
+// Register the element with the browser
+const cElements = customElements ?? window?.customElements;
+
+if (!cElements) {
+  throw new Error('Custom Elements not supported');
+}
+
+if (!cElements.get('pap-select')) {
+  cElements.define('pap-select', Select);
+}
+if (!cElements.get('pap-option')) {
+  cElements.define('pap-option', Option);
+}
